b0dea3d0088954880e853f8dd44a6bf743dcc80d
Konstantin Tcholokachvili <tcholoka@gmail.com>
2012-10-20 15:07:43 +0200

    Added the license file in a separated file, and fixed a typo: test_suite -> test_suite

--- py2neko.py
+++ py2neko.py
@@ -1,30 +1,6 @@
 #!/usr/bin/env python3
 
 """
-The MIT License
-
-Copyright (c) 2011-2012 Konstantin Tcholokachvili
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
-
-"""
 CREDITS:
 1. Parts of signature() and body() are courtesy of codegen.py
 	copyright: Copyright 2008 by Armin Ronacher.
@@ -117,7 +93,7 @@ class Py2Neko(ast.NodeVisitor):
 	    "setattr":"NOT_IMPLEMENTED",
 	    "slice":"NOT_IMPLEMENTED",
 	    "sorted":"NOT_IMPLEMENTED",
-	    "staticmethod":"NOT_IMPLEMENTED",
+	    "staticmethod": "NOT_IMPLEMENTED",
 	    "str":"NOT_IMPLEMENTED",
 	    "sum":"NOT_IMPLEMENTED",
 	    "super":"NOT_IMPLEMENTED",
